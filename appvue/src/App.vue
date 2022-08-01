d60f91cd77dd39711d04cadf733c2962c213790c
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2022-08-01 16:52:01 +0300

    app ready

---
+++ appvue/src/App.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="container">
+    <Header  @toggle="toggle" title="Task Tracker" :showAddTask = "showAddTask" ></Header>
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+    
+    <Tasks @delete-task="deleteTask" @toggle-reminder="toggleReminder" :tasks="tasks" />
+  </div>
+  
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Tasks from "./components/Tasks.vue";
+import AddTask from "./components/AddTask.vue";
+
+
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Tasks,
+    AddTask,
+   
+},
+  data(){
+    return{
+      tasks:[],
+      showAddTask: false,
+    }
+  },
+  methods:{
+    addTask(newTask){
+      this.tasks = [...this.tasks, newTask ]
+
+    },
+
+    deleteTask(id){
+      if(confirm("Are you sure?")){
+        this.tasks = this.tasks.filter((task)=>task.id !== id)
+      }
+    },
+
+    toggle(){
+      this.showAddTask = !this.showAddTask;
+
+    },
+
+    toggleReminder(id){
+      const tempArr = [];
+      this.tasks.forEach(e => {
+        if(e.id !== id){
+          tempArr.push(e)
+        } else{
+          const obj = {
+            id:e.id,
+            text:e.text,
+            day:e.day,
+            reminder: e.reminder? false:true
+          }
+          tempArr.push(obj)
+        }
+      });
+      this.tasks = tempArr;
+    }
+  },
+  created(){
+    this.tasks = [
+      {
+        id:1,
+        text: "Doctors Appointment",
+        day: "March 1st at 2:30pm",
+        reminder:true,
+      },
+      {
+        id:2,
+        text: "Meeting at School",
+        day: "March 3rd at 1:30pm",
+        reminder:true,
+      },
+      {
+        id:3,
+        text: "Food shopping",
+        day: "March 3st at 11:00am",
+        reminder:false,
+      },
+    ]
+  }
+
+}
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: 'Poppins', sans-serif;
+  }
+  .container {
+    max-width: 500px;
+    margin: 30px auto;
+    overflow: auto;
+    min-height: 300px;
+    border: 1px solid steelblue;
+    padding: 30px;
+    border-radius: 5px;
+  }
+  .btn {
+    display: inline-block;
+    background: #000;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    margin: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    font-size: 15px;
+    font-family: inherit;
+  }
+  .btn:focus {
+    outline: none;
+  }
+  .btn:active {
+    transform: scale(0.98);
+  }
+  .btn-block {
+    display: block;
+    width: 100%;
+  }
+</style>
---
+++ appvue/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button  
+            :title="showAddTask ? 'close Task': 'add Task'"  
+            :color="showAddTask ? 'red': 'green'" 
+            @btn-click="$emit('toggle')">
+        </Button>
+        <!-- <Button title="update Task" color="blue"></Button>
+        <Button title="Delete Task" color="red"></Button> -->
+    </header>
+</template>
+
+
+<script>
+    import Button from "./Button"
+
+    export default {
+        name: "Header",
+        props: {
+            title:String,
+            showAddTask:Boolean,
+            
+        },
+        components:{
+            Button
+        }
+    }
+</script>
+
+<style scoped>
+    header{
+        display:flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+
+</style>
